690c289f08f24476d862fb7d44b7e225f0c05c6c
Louis GALLET <louis.gallet@tumo.world>
2021-02-16 18:23:30 +0000

    create flappybird game and start flappy.py IA

--- flappy.py
+++ flappy.py
@@ -4,7 +4,7 @@ import time
 import os
 import random
 
-
+os.environ["SDL_VIDEODRIVER"] = "dummy"
 WIN_WIDTH = 600
 WIN_HEIGHT = 800
 BIRD_IMG = [pygame.transform.scale2x(
@@ -64,5 +64,45 @@ class Bird:
             self.img = self.IMGS[0]
         elif self.img_count < self.ANIMATION_TIME*2:
             self.img = self.IMGS[1]
-        elif self.img_count < self.ANIMATION_TIME:
+        elif self.img_count < self.ANIMATION_TIME*3:
+            self.img = self.IMGS[2]
+        elif self.img_count < self.ANIMATION_TIME*4:
+            self.img = self.IMGS[1]
+        elif self.img_count < self.ANIMATION_TIME*4 + 1:
             self.img = self.IMGS[0]
+            self.img_count = 0
+
+        if self.tilt <= -80:
+            self.img = self.IMGS[1]
+            self.img_count = self.ANIMATION_TIME*2
+
+        rotated_image = pygame.transform.rotate(self.img, self.tilt)
+        new_rect = rotated_image.get_rect(
+            center=self.img.get-rect(topleft=(self.x, self.y)).center)
+        win.blit(rotated_image, new_rect.topleft)
+
+    def get_mask(self):
+        return pygame.mask.from_surface(self.img)
+
+
+def draw_window(win, bird):
+    win.blit(BG_IMG, (0, 0))
+    bird.draw(win)
+    pygame.display.update()
+
+
+def main():
+    bird = Bird(200, 200)
+    win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        draw_window(win, bird)
+    pygame.quit()
+    quit()
+
+
+main()
